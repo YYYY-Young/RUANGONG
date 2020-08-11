63d4a574d7f2498ca9527a5251cc8d0b0ecbeb2f
no-dream <lrf123@buaa.edu.cn>
2020-08-11 13:34:08 +0800

    start2

--- src/main/java/com/diamond/controller/UserController.java
+++ src/main/java/com/diamond/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.diamond.controller;
 
+import com.diamond.entity.User;
 import com.diamond.result.Result;
 import com.diamond.result.ResultFactory;
 import com.diamond.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * @Classname UserController
@@ -20,6 +23,23 @@ public class UserController {
     @GetMapping("/api/admin/user")
     public Result listUsers() {
         return ResultFactory.buildSuccessResult(userService.userList());
+    }
+    @PutMapping("/api/admin/user/status")
+    public Result updateUserStatus(@RequestBody @Valid User requestUser) {
+        userService.updateUserStatus(requestUser);
+        return ResultFactory.buildSuccessResult("用户状态更新成功");
+    }
+
+    @PutMapping("/api/admin/user/password")
+    public Result resetPassword(@RequestBody @Valid User requestUser) {
+        userService.resetPassword(requestUser);
+        return ResultFactory.buildSuccessResult("重置密码成功");
+    }
+
+    @PutMapping("/api/admin/user")
+    public Result editUser(@RequestBody @Valid User requestUser) {
+        userService.editUser(requestUser);
+        return ResultFactory.buildSuccessResult("修改用户信息成功");
     }
 
 }
